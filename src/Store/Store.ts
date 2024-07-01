2c6ff16473b5051ff46bd84254cf29e486f13001
Neit03 <nthanhtien03@gmail.com>
2024-07-01 13:27:59 +0700

    Update

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -3,17 +3,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import { sponsorApi } from "../Features/Sponsor/sponsorApi";
 import { eventApi } from "../Features/Event/eventApi";
 import { sponsor_programApi } from "../Features/Sponsor/sponsor_programApi";
+import { orderApi } from "../Types/order.type";
 
 export const store = configureStore({
   reducer: {
     [sponsorApi.reducerPath]: sponsorApi.reducer,
     [eventApi .reducerPath]: eventApi.reducer,
     [sponsor_programApi.reducerPath] : sponsor_programApi.reducer,
+    [orderApi.reducerPath]: orderApi.reducer,
+
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(sponsorApi.middleware,eventApi.middleware,sponsor_programApi.middleware),
+    getDefaultMiddleware().concat(sponsorApi.middleware,eventApi.middleware,sponsor_programApi.middleware,orderApi.middleware),
   
 });
 
 export type RootState = ReturnType<typeof store.getState>; 
 export type AppDispatch = typeof store.dispatch;
+  
